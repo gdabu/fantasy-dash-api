6f14f3ec49d3c1cff70199a264d179f345a6d70f
geoff <geoffdabu@gmail.com>
2020-02-23 14:12:20 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,69 @@
+"use strict";
+
+const Hapi = require("@hapi/hapi");
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: "localhost"
+  });
+
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      return [
+        {
+          playerFirstName: "Damian",
+          playerLastName: "Lillard",
+          number: "0",
+          currentTeam: "POR",
+          seasons: [
+            {
+              year: 2019,
+              team: "POR",
+              stats: {
+                GAMES_PLAYED: 54,
+                POINTS: 1594,
+                THREE_POINTS: 212,
+                ASSISTS: 429,
+                REOUNDS: 235,
+                STEALS: 52,
+                BLOCKS: 18,
+                FIELD_GOAL_PCT: 46,
+                FREE_THROW_PCT: 88.9,
+                TURN_OVERS: 158
+              }
+            },
+            {
+              year: 2018,
+              team: "POR",
+              stats: {
+                GAMES_PLAYED: 54,
+                POINTS: 1594,
+                THREE_POINTS: 212,
+                ASSISTS: 429,
+                REOUNDS: 235,
+                STEALS: 52,
+                BLOCKS: 18,
+                FIELD_GOAL_PCT: 46,
+                FREE_THROW_PCT: 88.9,
+                TURN_OVERS: 158
+              }
+            }
+          ]
+        }
+      ];
+    }
+  });
+
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
+};
+
+process.on("unhandledRejection", err => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
