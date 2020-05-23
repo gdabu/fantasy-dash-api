e5dc75dafff3b8d3d24ce30e7571aa42f6c7ab5f
geoff <geoffdabu@gmail.com>
2020-05-22 20:25:30 -0700

    added jest and base test file for dash-nhl routes

---
+++ tests/dash-nhl.test.js
@@ -0,0 +1,9 @@
+const app = require('../app'); // Link to your server file
+const supertest = require('supertest');
+const request = supertest(app);
+
+const league = 'nhl';
+
+it('', async (done) => {
+  done();
+});
