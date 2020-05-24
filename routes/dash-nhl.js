7a424dd062240702241780594c597def04f0bd52
geoff <geoffdabu@gmail.com>
2020-05-23 18:47:14 -0700

    added function header comments to detail the functions behaviour

--- routes/dash-nhl.js
+++ routes/dash-nhl.js
@@ -8,6 +8,10 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Fantasy Dash API - NHL' });
 });
 
+/**
+ * /getAllTeams
+ * Gets a list of all teams currently in the league.
+ */
 router.get('/getAllTeams', async function (req, res) {
   let { season } = req.query;
 
@@ -24,6 +28,11 @@ router.get('/getAllTeams', async function (req, res) {
   res.status(status).json(payload);
 });
 
+/**
+ * /getTeam
+ * Gets a specified team.
+ * @queryparam teamId (Required)
+ */
 router.get('/getTeam', async function (req, res) {
   let { teamId } = req.query;
 
@@ -43,6 +52,12 @@ router.get('/getTeam', async function (req, res) {
   res.status(status).json(payload);
 });
 
+/**
+ * /getRoster
+ * Gets a list of all the players of a specified team for a specified season.
+ * @queryparam teamId (Required)
+ * @queryparam season
+ */
 router.get('/getRoster', async function (req, res) {
   let { teamId, season } = req.query;
 
@@ -69,6 +84,11 @@ router.get('/getRoster', async function (req, res) {
   res.status(status).json(payload);
 });
 
+/**
+ * /getAllRosteredPlayers
+ * Gets a list of all players who were on any team roster for a specified season.
+ * @queryparam season
+ */
 router.get('/getAllRosteredPlayers', async function (req, res) {
   let { season } = req.query;
 
@@ -85,32 +105,11 @@ router.get('/getAllRosteredPlayers', async function (req, res) {
   res.status(status).json(payload);
 });
 
-router.get('/getRosterPlayersFull', async function (req, res) {
-  let { teamId, season } = req.query;
-
-  if (teamId === undefined) {
-    res.status(400).json(`400 - :teamId can't be empty`);
-    return;
-  }
-
-  if (isNaN(teamId)) {
-    res.status(400).json('400 - :teamId needs to be a number');
-    return;
-  }
-
-  if (season !== undefined && !RegExp('\\d{8}').test(season)) {
-    res
-      .status(400)
-      .json('400 - :season needs to follow proper format - yyyyyyyy (Example: 20192020)');
-    return;
-  }
-
-  let { status, payload } = await NHL_API.getRosterPlayersFull(teamId, season).catch((error) => {
-    return error;
-  });
-  res.status(status).json(payload);
-});
-
+/**
+ * /getPlayerInfo
+ * Gets the full info list of a specified player
+ * @queryparam playerId (Required)
+ */
 router.get('/getPlayerInfo', async function (req, res) {
   let { playerId } = req.query;
 
@@ -130,6 +129,12 @@ router.get('/getPlayerInfo', async function (req, res) {
   res.status(status).json(payload);
 });
 
+/**
+ * /getPlayerStats
+ * Gets the stats of a specified player for a specified season
+ * @queryparam playerId (Required)
+ * @queryparam season
+ */
 router.get('/getPlayerStats', async function (req, res) {
   let { playerId, season } = req.query;
 
@@ -156,6 +161,12 @@ router.get('/getPlayerStats', async function (req, res) {
   res.status(status).json(payload);
 });
 
+/**
+ * /getPlayer
+ * Gets the full info list of a specified player, and their stats for the specified season.
+ * @queryparam playerId (Required)
+ * @queryparam season
+ */
 router.get('/getPlayer', async function (req, res) {
   let { playerId, season } = req.query;
 
@@ -177,6 +188,39 @@ router.get('/getPlayer', async function (req, res) {
   }
 
   let { status, payload } = await NHL_API.getPlayer(playerId, season).catch((error) => {
+    return error;
+  });
+  res.status(status).json(payload);
+});
+
+/**
+ * /getRosterPlayersFull
+ * Gets a list of all players who were on a specified team for a specified season. The players full information and
+ * stats for the specified season will be included.
+ * @queryparam teamId (Required)
+ * @queryparam season
+ */
+router.get('/getRosterPlayersFull', async function (req, res) {
+  let { teamId, season } = req.query;
+
+  if (teamId === undefined) {
+    res.status(400).json(`400 - :teamId can't be empty`);
+    return;
+  }
+
+  if (isNaN(teamId)) {
+    res.status(400).json('400 - :teamId needs to be a number');
+    return;
+  }
+
+  if (season !== undefined && !RegExp('\\d{8}').test(season)) {
+    res
+      .status(400)
+      .json('400 - :season needs to follow proper format - yyyyyyyy (Example: 20192020)');
+    return;
+  }
+
+  let { status, payload } = await NHL_API.getRosterPlayersFull(teamId, season).catch((error) => {
     return error;
   });
   res.status(status).json(payload);
