9a562ea486bbe1e0479e5282709e277df8876829
geoff <geoffdabu@gmail.com>
2020-06-06 13:02:51 -0700

    changed var to let. more semantically correct.

--- routes/dash-nhl.js
+++ routes/dash-nhl.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 const NHL_API = require('../connectors/NHL_API.js');
 
