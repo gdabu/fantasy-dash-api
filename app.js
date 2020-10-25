06ba27744906250a8340e955f7f6feaa917f25c7
geoff <geoffdabu@gmail.com>
2020-10-24 20:07:11 -0700

    switched back to setting headers instead of cors lib. cors lib was breaking tests

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ var dashNhlRouter = require('./routes/nhl-stats');
 
 var app = express();
 
-app.use(cors);
+// app.use(cors);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
